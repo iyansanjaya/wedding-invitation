91062bf18e98887521e221e60d2d155fd7d2ad7d
Iyan Sanjaya <iyan.sanjaya1@gmail.com>
2025-02-21 23:10:20 +0700

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/template/theme-provider";
-import Footer from "@/components/organisms/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
