9ad5527f24a3699b555c639189a3f8339d6df4f6
Iyan Sanjaya <iyan.sanjaya1@gmail.com>
2025-02-21 20:54:13 +0700

    update

--- src/app/tommy-enjelika/page.tsx
+++ src/app/tommy-enjelika/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { BlurFade } from "@/components/magicui/blur-fade";
 
 export default function Pages() {
   return (
@@ -26,7 +27,7 @@ export default function Pages() {
             width: "100%",
             height: "auto",
           }}
-          className="absolute bottom-0 z-30"
+          className="absolute bottom-0 z-10"
         />
         <Image
           src="/img/avatar.png"
@@ -37,29 +38,39 @@ export default function Pages() {
           style={{
             height: "auto",
           }}
-          className="absolute bottom-20 z-30 w-[86%] md:w-[70%]"
+          className="absolute bottom-20 z-10 w-[86%] md:w-[70%]"
         />
-        <div className="flex flex-col justify-center pb-[70%] items-center gap-5 text-[12px] md:text-base text-center px-8 md:px-24 z-20">
-          <p className="mt-[14%] md:mt-[15%] text-[13px] md:text-base px-4 py-1 border rounded-full uppercase font-bold">
-            Sangjit & Pemberkatan
-          </p>
-          <div className="space-y-1 text-center">
-            <h1 className="px-[66px] font-bold text-2xl md:text-3xl">
-              ENJELIKA
-            </h1>
-            <h1 className="px-[66px] font-bold text-2xl md:text-3xl">&</h1>
-            <h1 className="px-[66px] font-bold text-2xl md:text-3xl text-center">
-              OEY, TOMMI (DARTO)
-            </h1>
-          </div>
-          <p className="text-sm">Minggu, 16 Maret 2025</p>
-          <p>
-            &quot;Bila dalam perjalanan hidupmu, engkau menemukan seorang teman
-            yang bijaksana dan cocok untuk hidup denganmu, hendaklah engkau
-            berjalan bersama dengan gembira dan penuh kesadaran mengatasi segala
-            bahaya&quot;
-          </p>
-          <p>(Dhammapada XXIII-328)</p>
+        <div className="flex flex-col justify-center pb-[70%] pt-10 md:pt-[16%] items-center gap-5 text-[12px] md:text-base text-center px-8 md:px-24 z-20">
+          <BlurFade delay={0.25} inView>
+            <p className="mt-[14%] md:mt-[15%] text-[13px] md:text-base px-4 py-1 border rounded-full uppercase font-bold">
+              Sangjit & Pemberkatan
+            </p>
+          </BlurFade>
+          <BlurFade delay={0.5} inView>
+            <div className="space-y-1 text-center">
+              <h1 className="px-[66px] font-bold text-2xl md:text-3xl">
+                ENJELIKA
+              </h1>
+              <h1 className="px-[66px] font-bold text-2xl md:text-3xl">&</h1>
+              <h1 className="px-[66px] font-bold text-2xl md:text-3xl text-center">
+                OEY, TOMMI (DARTO)
+              </h1>
+            </div>
+          </BlurFade>
+          <BlurFade delay={0.75} inView>
+            <p className="text-sm">Minggu, 16 Maret 2025</p>
+          </BlurFade>
+          <BlurFade delay={1} inView>
+            <p>
+              &quot;Bila dalam perjalanan hidupmu, engkau menemukan seorang
+              teman yang bijaksana dan cocok untuk hidup denganmu, hendaklah
+              engkau berjalan bersama dengan gembira dan penuh kesadaran
+              mengatasi segala bahaya&quot;
+            </p>
+          </BlurFade>
+          <BlurFade delay={1.25} inView>
+            <p>(Dhammapada XXIII-328)</p>
+          </BlurFade>
         </div>
       </div>
     </main>
